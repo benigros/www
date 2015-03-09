80b29e693342cde551a993b7552c88186acde485
benigros <bdevilliers@ensc.fr>
2015-03-09 17:18:49 +0100

    Create Creerevent.php

---
+++ Creerevent.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+         <link rel="stylesheet" href="css/mycss.css" />
+    
+    </head>
+    <body>
+        
+         
+       <div id="start">
+       <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Connexion.php">Connexion</a></li>
+   <a href="C:file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/inscription.php">Inscription</a></li>
+</div>
+        <br/>
+             
+           <header id='header'>
+       <nav id="nav">
+  <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Accueil.php">Accueil</a></li>
+   <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Créerevent.php">Créer un évènement</a></li>
+   <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/RechercheEvent.php">Rechercher un évènement</a></li>
+ <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/RechercheGroupe.php">Rechercher un groupe</a></li>
+<a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Profil.php">Mon profil</a></li>
+</nav>
+           </header>
+        <h1> Création d'évènement</h1>
+        
+        <form method='post' action=''>
+        
+        <fieldset>
+        <label for="datevent">Date </label> : <input type="date" name="datevent" id="date" value="" required/><br/><br/>
+        
+    <label for="NomEvent">Nom</label> : <input type="text" name="NomEvent" id="NomEvent" placeholder="Apéro à la colloc'" required/><br/><br/>
+    
+    
+    <label for="TypeEvent">Type d'évènement :</label>
+    <select name="TypeEvent">
+
+    <option value="Anniversaire">Anniversaire</option>
+
+    <option value="Cocktail">Cocktail</option>
+
+    <option value="concert">Concert</option>
+
+    <option value="Enterrement de vie de J-F et J-H">Enterrement de vie de J-F et J-H</option>
+    
+    <option value="exposition">Exposition</option>
+    
+    <option value="mariage">Mariage</option>
+    
+     <option value="reception">Réception</option>
+     
+      <option value="sortie culturelle">Sortie culturelle</option>
+       <option value="soiree etudiante">Soirée étudiante</option>
+        <option value="spectacle">spectacle</option>
+         <option value="conference">conference</option>
+          <option value="congres">Congrès</option>
+           <option value="formation">Formation</option>
+            <option value="fete">Fête</option>
+    
+    
+    </select>
+            </fieldset>
+
+
+<input type="submit" value="Publier" />
+    
+    
+        <?php
+        // put your code here
+        ?>
+    </form>
+    </body>
+</html>
