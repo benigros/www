a019beffadc08b623b56891457f14b495e74456f
benigros <bdevilliers@ensc.fr>
2015-03-09 17:22:17 +0100

    Create inscription.php

---
+++ inscription.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Inscription eventour</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/mycss.css" />
+    </head>
+    <body>
+        
+         <div id="start">
+       <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Connexion.php">Connexion</a></li>
+   <a href="C:file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/inscription.php">Inscription</a></li>
+</div>
+        <br/>
+             
+           <header id='header'>
+       <nav id="nav">
+  <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Accueil.php">Accueil</a></li>
+   <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Créerevent.php">Créer un évènement</a></li>
+   <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/RechercheEvent.php">Rechercher un évènement</a></li>
+ <a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/RechercheGroupe.php">Rechercher un groupe</a></li>
+<a href="file:///C:/Users/Benigro%27s/Documents/NetBeansProjects/PhpWeb/Profil.php">Mon profil</a></li>
+</nav>
+           </header>
+        
+        <h1>Inscription</h1>
+
+<form method="post" action="traitementinsc.php">
+   
+    <fieldset>
+        <legend>Informations de base        </legend>
+
+        
+    <label for="Prenom">Prénom</label> : <input type="text" name="Prenom" id="Prenom" placeholder="Carl" required/><br/><br/>
+    <label for="Nom">Nom</label> : <input type="text" name="Nom" id="Nom" placeholder="Moscet" required/><br/><br/>
+    <label for="newmdp">Date de naissance</label> : <input type="date" name="date" id="date" value="1994-01-25" required/><br/><br/>
+    
+        </fieldset>
+
+    <fieldset>
+        <legend>Informations complémentaires       </legend>
+    <label for="adresse">Adresse électronique</label> : <input type="email" name="adresse" id="adresse" placeholder="toto@xxx.fr" required/><br/><br/>
+    <label for="confadresse">Confirmer l'adresse</label> : <input type="email" name="confadresse" id="confadresse" placeholder="toto@xxx.fr" required/><br/><br/>
+    <label for="mdp">Nouveau mot de passe</label> : <input type="password" name="mdp" id="mdp" placeholder="mdp" required/><br/><br/>
+    <label for="newmdp">Confirmer le mot de passe</label> : <input type="password" name="confmdp" id="confmdp" placeholder="mdp" required/><br/><br/>
+    
+    </fieldset>
+    <input type="submit" value="Inscription" />
+    
+    
+     
+          
+     
+</form>
+
+    </body>
+</html>
