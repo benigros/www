3ae624a45bd117355659f7b2a93ff088cd681399
benigros <bdevilliers@ensc.fr>
2015-03-09 17:02:58 +0100

    eyo
    jai changé des trucs

---
+++ traitementco.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+<p> votre adresse et votre mot de passe sont :
+<?php
+echo $_POST['adresse'];
+echo $_POST['adresse'];
+echo $_POST['Prénom'];
+?>
+<p>Si tu veux changer tes iden, <a href="Connexion.php">clique ici</a> pour revenir à la page connexion.</p>
+</body>
+</html>
