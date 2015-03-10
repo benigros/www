b4774b77e77e8e6e479fd62ec2730bc70d872aca
benigros <bdevilliers@ensc.fr>
2015-03-10 12:04:39 +0100

    Update traitementco.php

--- traitementco.php
+++ traitementco.php
@@ -12,15 +12,51 @@ include("identifiants.php");
 <meta charset="UTF-8">
 <title></title>
 </head>
-<body>
-<p> votre adresse et votre mot de passe sont :
-<?php
+
+<?php 
+
+$adresse= $_POST['adresse'];
+
+$req1 = "SELECT * FROM logs WHERE adresse = '".$_POST['adresse']."'";
+$res1 = mysql_query($req1);
+$nbres1=mysql_num_rows($res1);
+
+if ($nbres1 != 0)
+{
+	$req2 = "SELECT * FROM logs WHERE adresse = '".$_POST['adresse']."' AND mdp = '".$_POST['mdp']."'";
+	$res2 = mysql_query($req2);
+	$nbres2=mysql_num_rows($res2);
+	
+	
+	if ( $nbres2 != 0)
+	{
+		
+		
+		$_SESSION["adresse"]=$_POST['adresse'];
+		$req3 = "SELECT id FROM logs WHERE adresse = '".$_POST['adresse']."' AND mdp = '".$_POST['mdp']."'";
+		$res3=mysql_query($req3);
+		$iduser=mysql_result($res3,0,'id');
+		$_SESSION['id']=$iduser;
+		
+		
+		header( 'Location: RechercheEvent.php');
+		
+	}
+	else{
+		header( 'Location: Connexion.php');
+		
+	}
+}
 
 
-$marequete = "INSERT  INTO logs VALUES ( '".$_POST['adresse']."','".$_POST['mdp']."')  ";
 
+
+
+
+mysql_close($link);
 
 ?>
+<body>
 <p>Si tu veux changer tes iden, <a href="Connexion.php">clique ici</a> pour revenir à la page connexion.</p>
 </body>
 </html>
